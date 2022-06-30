78f45254013ffa01ec0f5fa0bcf7b1fbb365e02e
diegorodd <dieegoa@icloud.com>
2022-06-30 17:48:32 -0400

    licenses

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,40 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+
+
+![License](https://img.shields.io/badge/label-${data.license_badge}-informational.svg)
+
+## Description 
+${data.description}
+
+## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  ${data.installation_instructions}
+  
+  ## Usage
+  ${data.usage_information}
+  
+  ## License
+  This application is covered under ${data.license}.
+  
+  ## Contributing
+  ${data.contribution_guidelines}
+  
+  ## Tests
+  ${data.testing_information}
+  
+  ## Questions
+  Have questions? Contact the developer at ${data.email} or [check out their GitHub profile](https://github.com/${data.github_username}).
+
 `;
 }
 
