bbc9865f642613a590ce4d004cb4764c294ff8ae
diegorodd <dieegoa@icloud.com>
2022-06-30 23:20:01 -0400

    questions

--- index.js
+++ index.js
@@ -6,7 +6,39 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+    type: "input",
+    name: "Title",
+    message: "What is the title of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Could you add some decription?",
+    },
+    {
+         type: "input",
+         name: "License",
+         message: "What license does your project have?",
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "what are the usage of this readME?",
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your username in github?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Whats your email?",
+            },
+
+];
 
 
 // TODO: Create a function to write README file
